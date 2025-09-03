8626c33f92e1c33a5d37f952be9aaa74693fbbb2
Pavel-Tomilov <pasha.tomiloff@gmail.com>
2025-09-03 23:27:03 +0300

    поправил структуру папок

--- src/components/ItemList/ItemList.tsx
+++ src/components/ItemList/ItemList.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./ItemList.css";
-import ItemCard from "../../ItemCard/ItemCard";
+import ItemCard from "../ItemCard/ItemCard";
 
 interface ItemListProps {
   items: string[];
